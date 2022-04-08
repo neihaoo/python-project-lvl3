d74cb725a2a0dc635d4b39769bcaaf1acb9cbdf1
neihaoo <neihaoo@gmail.com>
2022-04-08 20:57:36 +0300

    add download for all local assets

--- page_loader/download.py
+++ page_loader/download.py
@@ -2,7 +2,6 @@
 
 import os
 import re
-from typing import Tuple
 from urllib.parse import urljoin, urlparse
 
 import requests
@@ -10,80 +9,93 @@ from bs4 import BeautifulSoup
 
 DEFAULT_DST_FOLDER = os.getcwd()
 
+links_attrs = {
+    'img': 'src',
+    'link': 'href',
+    'script': 'src',
+}
 
-def check_domain(outer_host: str, inner_host: str) -> bool:
+
+def has_attr(tag: str) -> bool:
+    """Check if tag has attrs 'src' or 'href'."""
+    return tag.has_attr('src') or tag.has_attr('href')
+
+
+def is_local_domain(outer_host: str, inner_host: str) -> bool:
     """Check local assets for local domain."""
-    url_parts = urlparse(inner_host)
+    outer_url_parts = urlparse(outer_host)
+    inner_url_parts = urlparse(inner_host)
 
-    return url_parts.hostname is None or outer_host in url_parts.hostname
-
-
-def parse_url(url: str) -> Tuple:
-    """Split url on host and path."""
-    url_parts = urlparse(url)
-
-    return (url_parts.hostname, url_parts.path[1:])
+    return (
+        inner_url_parts.hostname is None
+        or outer_url_parts.hostname == inner_url_parts.hostname
+    )
 
 
-def make_name(url) -> str:
+def make_filename(url) -> str:
     """Make name for assets."""
-    hostname, path = parse_url(url)
-    name, extension = os.path.splitext(path)
+    url_parts = urlparse(url)
+    filename, extension = os.path.splitext(url_parts.path[1:])
 
-    name = re.sub(r'\W', '-', os.path.join(hostname, name))
+    filename = re.sub(r'\W', '-', os.path.join(url_parts.hostname, filename))
     extension = extension if extension else '.html'
 
-    return '{0}{1}'.format(name, extension)
+    return '{0}{1}'.format(filename, extension)
 
 
-def make_folder(filename: str, dst: str) -> str:
+def make_folder(url: str, dst: str) -> str:
     """Create folder for assets and return folder name."""
-    foldername = filename.replace('.html', '_files')
-    os.mkdir(os.path.join(dst, foldername))
+    filename, _ = os.path.splitext(make_filename(url))
+    folder_name = '{0}{1}'.format(filename, '_files')
 
-    return foldername
+    os.mkdir(os.path.join(dst, folder_name))
+
+    return folder_name
 
 
-def download_assets(src: str, dst: str) -> None:
-    """Download assets for page."""
-    req = requests.get(src, stream=True)
-    assert_name = make_name(src)
+def download_asset(url: str, dst: str) -> str:
+    """Download asset for page."""
+    req = requests.get(url, stream=True)
+    asset_name = make_filename(url)
 
-    with open(os.path.join(dst, assert_name), 'wb') as filename:
+    with open(os.path.join(dst, asset_name), 'wb') as filename:
         filename.write(req.content)
 
-    return assert_name
+    return asset_name
 
 
-def parse_html(url: str, dst: str, html: str) -> Tuple:
+def parse_html(url: str, dst: str, html: str) -> str:
     """Parse html file."""
-    html_name = make_name(url)
-
     soup = BeautifulSoup(html, 'html.parser')
     assets = (
         asset
-        for asset in soup.find_all('img')
-        if check_domain(url, asset.get('src'))
+        for asset in soup.find_all(['img', 'link', 'script'])
+        if is_local_domain(url, asset.get(links_attrs[asset.name]))
     )
 
     if assets:
-        assets_dst = make_folder(html_name, dst)
+        assets_dst = make_folder(url, dst)
 
         for asset in assets:
-            asset_name = download_assets(
-                urljoin(url, asset.get('src')),
+            asset_name = download_asset(
+                urljoin(url, asset.get(links_attrs[asset.name])),
                 os.path.join(dst, assets_dst),
             )
 
-            asset['src'] = os.path.join(assets_dst, asset_name)
+            asset[links_attrs[asset.name]] = os.path.join(
+                assets_dst,
+                asset_name,
+            )
 
-    return html_name, soup.prettify()
+    return soup.prettify()
 
 
-def download(src: str, dst: str = DEFAULT_DST_FOLDER) -> str:
+def download(url: str, dst: str = DEFAULT_DST_FOLDER) -> str:
     """Download html page to dst folder."""
-    req = requests.get(src)
-    html_name, html_data = parse_html(src, dst, req.text)
+    req = requests.get(url, stream=True)
+
+    html_name = make_filename(url)
+    html_data = parse_html(url, dst, req.text)
 
     with open(os.path.join(dst, html_name), 'w') as filename:
         filename.write(html_data)
--- tests/test_download.py
+++ tests/test_download.py
@@ -1,83 +1,84 @@
 import os
 import tempfile
+import requests_mock as req_mock
 
 from page_loader import download
+from page_loader.download import download_asset
 
 DIR_PATH = os.path.dirname(__file__)
 SOURCE_PATH = 'https://ru.hexlet.io/courses'
-ASSERTS_PATH = 'https://ru.hexlet.io/assets/professions/python.png'
-FIXTURE_NAME = {
+
+fixture_names = {
     'html_after': 'ru-hexlet-io-courses.html',
     'html_before': 'courses.html',
     'image': 'ru-hexlet-io-assets-professions-python.png',
+    'css': 'ru-hexlet-io-assets-application.css',
+    'js': 'ru-hexlet-io-packs-js-runtime.js',
     'folder': 'ru-hexlet-io-courses_files',
 }
 
 
-def get_fixture_path(filename, subpath=''):
-    return os.path.join(DIR_PATH, 'fixtures', subpath, filename)
+def get_fixture_path(filename):
+    return os.path.join(DIR_PATH, 'fixtures', filename)
 
 
-def read_file(filename, subpath='', mode='r'):
-    return open(get_fixture_path(filename, subpath), mode).read()
+def read_file(filename):
+    return open(get_fixture_path(filename)).read()
 
 
-before = read_file(FIXTURE_NAME['html_before'], 'before')
-after = read_file(FIXTURE_NAME['html_after'], 'after')
-image = read_file(
-    FIXTURE_NAME['image'],
-    os.path.join('after', FIXTURE_NAME['folder']),
-    'rb',
-)
+before = read_file('before.html')
+
+expected_html = read_file('after.html')
+expected_structure = [
+    [
+        fixture_names['folder'],
+        [
+            fixture_names['image'],
+            fixture_names['js'],
+            fixture_names['css'],
+            fixture_names['html_after'],
+        ],
+    ],
+    fixture_names['html_after'],
+]
 
 
-def check_entry(entry):
-    return entry.name if entry.is_file() else [entry.name, list_dir(entry)]
+def get_node(entry):
+    return (
+        entry.name if entry.is_file() else [entry.name, build_dir_tree(entry)]
+    )
 
 
-def list_dir(dir_path):
+def build_dir_tree(dir_path):
     with os.scandir(dir_path) as it:
-        return [check_entry(entry) for entry in it]
+        return [get_node(entry) for entry in it]
 
 
 def test_download(requests_mock):
-    requests_mock.get(SOURCE_PATH, text=before)
-    requests_mock.get(
-        'https://ru.hexlet.io/assets/professions/python.png',
-        content=image,
-    )
+    requests_mock.get(req_mock.ANY, text=before)
 
     with tempfile.TemporaryDirectory() as tmpdirname:
-        expected = os.path.join(tmpdirname, FIXTURE_NAME['html_after'])
+        expected = os.path.join(tmpdirname, fixture_names['html_after'])
         actual = download(SOURCE_PATH, tmpdirname)
 
         assert expected == actual
 
 
-def test_download_html_data(requests_mock):
-    requests_mock.get(SOURCE_PATH, text=before)
-    requests_mock.get(
-        'https://ru.hexlet.io/assets/professions/python.png',
-        content=image,
-    )
+def test_html_data(requests_mock):
+    requests_mock.get(req_mock.ANY, text=before)
 
     with tempfile.TemporaryDirectory() as tmpdirname:
-        actual = download(SOURCE_PATH, tmpdirname)
+        actual_html = download(SOURCE_PATH, tmpdirname)
 
-        assert read_file(actual) == after
+        assert expected_html == read_file(actual_html)
 
 
-def test_download_structire(requests_mock):
-    requests_mock.get(SOURCE_PATH, text=before)
-    requests_mock.get(
-        'https://ru.hexlet.io/assets/professions/python.png',
-        content=image,
-    )
+def test_dst_structire(requests_mock):
+    requests_mock.get(req_mock.ANY, text=before)
 
     with tempfile.TemporaryDirectory() as tmpdirname:
         download(SOURCE_PATH, tmpdirname)
 
-        actual = list_dir(tmpdirname)
-        expected = list_dir(os.path.join(DIR_PATH, 'fixtures/after'))
+        actual_structure = build_dir_tree(tmpdirname)
 
-        assert expected == actual
+        assert expected_structure == actual_structure
